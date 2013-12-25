0da1ebbd2fd9464b633ba444eaf31a5066384768
Jonatan Littke <jonatan.littke@gmail.com>
2013-12-25 10:38:16 +0100

    better way of finding out who's turn it is.

--- app/client/collections.js
+++ app/client/collections.js
@@ -1,3 +1,20 @@
 Teams = new Meteor.Collection("teams");
 Characters = new Meteor.Collection("characters");
-Games = new Meteor.Collection("games");
+Games = new Meteor.Collection("games", {
+
+  transform: function (game) {
+    /*
+     * Calculate whose turn it is and save it on the game instance.
+     * Don't store this on the document since it's derived data.
+     */
+    var i=0;
+    while (!game.currentTurn) {
+      if (game.playedThisTurn.indexOf(game.teams[i]) === -1) {
+        game.currentTurn = game.teams[i];
+      }
+      i++;
+    };
+
+    return game;
+  }
+});

83feee430b83fe4fb2d6cd7650f03d35b2f6df7b
Jonatan Littke <jonatan.littke@gmail.com>
2013-12-26 22:20:44 +0100

    Start implementing stats.

--- app/collections.js
+++ app/collections.js
@@ -1,5 +1,25 @@
 Teams = new Meteor.Collection("teams");
-Characters = new Meteor.Collection("characters");
+Characters = new Meteor.Collection("characters", {
+  transform: function (character) {
+    var attrs = character.attributes;
+    /*
+     * Methods for getting stats, derived from attributes
+     */
+    character.getHitPoints = function() {
+      return attrs.strength * 100;
+    }
+
+    character.getMana = function() {
+      return attrs.mana ? attrs.mana * 100 : 0;
+    }
+
+    character.getStamina = function() {
+      return parseInt(10 * ((attrs.strength * 0.75) + (attrs.agility * 0.45) + (attrs.psyche * 0.12)), "10");
+    }
+
+    return character;
+  }
+});
 
 Games = new Meteor.Collection("games", {
 
